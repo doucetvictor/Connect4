2cae9993ca8776505581ce428dacaefa97e7445c
Florian Lauch <florian.lauch@epitech.eu>
2022-03-18 16:03:54 +0100

    [ADD] Options parsing

--- include/connect.h
+++ include/connect.h
@@ -19,9 +19,7 @@ typedef struct game_s {
 
 int connect(int argc, char **argv);
 int get_options(int argc, char **argv, game_t *game);
-
-void board_display(char **map);
-
 int get_player_choice(char c);
+void board_display(char **map);
 
 #endif
